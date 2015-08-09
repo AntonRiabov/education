e5815eb3bf7f7be4583397f1fee0076fa4e170ba
anton.riabov <anton.riabov@engagepoint.com>
2015-08-09 15:29:52 +0300

    added module for zookeeper learning

---
+++ ZooKeeper/src/main/java/com/paladii/school/zookeeper/JoinGroup.java
@@ -0,0 +1,11 @@
+package com.paladii.school.zookeeper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: PALADII
+ * Date: 09.08.15
+ * Time: 15:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class JoinGroup {
+}
